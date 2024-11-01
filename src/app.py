e95c3858abe319ceeb7f0e832face8ef6f2e2bed
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-01 09:53:45 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Nyerseg Data Room", layout="wide")
 
 st.title("Nyerseg Data Room")
-st.sidebar.title("Navigation")
-
-# Introduction text on the main page
-st.write("Welcome to the Nyerseg Data Room App! Use the sidebar to select a page and proceed with specific tasks.")
+st.write("Welcome to the Nyerseg Data Room App! Use the sidebar to navigate between pages.")
