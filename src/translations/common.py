3d022723573326ec11676e5ed67ad7dcd33e3522
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-10 15:05:32 +0100

    Create common.py

---
+++ src/translations/common.py
@@ -0,0 +1,38 @@
+common_translations = {
+    'en': {
+        'page_title': "Nyerseg Dataroom",
+        'welcome_message': """
+        **Welcome to the Nyerseg Dataroom by Hawkar Ali Abdulhaq, Szeged University**  
+        **Contact**: [hawkar.ali.abdulhaq@szte.hu](mailto:hawkar.ali.abdulhaq@szte.hu)
+        """,
+        'access_key_prompt': "Please enter the access key to proceed:",
+        'access_key_label': "Access Key",
+        'access_button': "Access",
+        'access_granted': "Access granted! You may now use the app.",
+        'invalid_key': "Invalid access key. Please try again.",
+        'navigation': "Navigation",
+        'home': "Home",
+        'shape_cleaning': "Shape Cleaning",
+        'generate_wells': "Generate Wells",
+        'clean_wells': "Clean Wells",
+        'download': "Download",
+    },
+    'hu': {
+        'page_title': "Nyerseg Adattár",
+        'welcome_message': """
+        **Üdvözöljük a Nyerseg Adattárban, Hawkar Ali Abdulhaq, Szegedi Egyetem**  
+        **Kapcsolat**: [hawkar.ali.abdulhaq@szte.hu](mailto:hawkar.ali.abdulhaq@szte.hu)
+        """,
+        'access_key_prompt': "Kérjük, adja meg a hozzáférési kulcsot a folytatáshoz:",
+        'access_key_label': "Hozzáférési kulcs",
+        'access_button': "Hozzáférés",
+        'access_granted': "Hozzáférés engedélyezve! Most már használhatja az alkalmazást.",
+        'invalid_key': "Érvénytelen hozzáférési kulcs. Kérjük, próbálja újra.",
+        'navigation': "Navigáció",
+        'home': "Kezdőlap",
+        'shape_cleaning': "Alak tisztítása",
+        'generate_wells': "Kutak generálása",
+        'clean_wells': "Kutak tisztítása",
+        'download': "Letöltés",
+    }
+}
